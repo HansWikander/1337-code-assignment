447017ddf7443b1c6d5ef7fe7e0e8d35ee8d1dbe
HansWikander <hans.wikander@andorit.se>
2022-11-11 10:46:26 +0100

    Add main program

---
+++ WebTraverser/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace WebTraverser
+{
+    internal class Program
+    {
+        private static string _website = "https://tretton37.com";
+        private static string _outputFolder = "result";
+
+        static void Main(string[] args)
+        {
+            if (args.Length > 0) 
+            {
+                _outputFolder = args[0];
+            }
+            if (args.Length > 1)
+            {
+                _website = args[1];
+            }
+
+            var traverser = new Traverser(_website, _outputFolder);
+
+            Console.WriteLine($"Starting traversing {_website} into {_outputFolder}");
+            try
+            {
+                Task.Run(async () => { await traverser.Run(); }).Wait();
+            }
+            catch (AggregateException exceptions)
+            {
+                Console.WriteLine($"ERROR");
+                foreach (var ex in exceptions.InnerExceptions)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            Console.WriteLine($"Done traversing");
+        }
+    }
+}
---
+++ WebTraverser/Traverser.cs
@@ -0,0 +1,15 @@
+﻿namespace WebTraverser
+{
+    internal class Traverser
+    {
+        public Traverser(string website, string outputFolder)
+        {
+
+        }
+
+        public async Task Run()
+        {
+            throw new NotImplementedException("TBD");
+        }
+    }
+}
