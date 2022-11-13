43db40834f4addb630e7405cf9bb2822a6860f78
HansWikander <hans.wikander@andorit.se>
2022-11-13 09:02:12 +0100

    Refactor to use more atomic methods

--- WebTraverser/Traverser.cs
+++ WebTraverser/Traverser.cs
@@ -19,6 +19,7 @@ namespace WebTraverser
 
         public async Task Run()
         {
+            _visitedPages.Clear();
             _visitedPages.Add(_website);
             await TraversePage(_website);
         }
@@ -35,26 +36,32 @@ namespace WebTraverser
 
         private IEnumerable<string> GetUnvisitedLinks(string page)
         {
-            var hrefPattern = @"<a href\s*=\s*(?:[""'](?<1>[^""']*)[""']|(?<1>[^>\s]+))";
+            foreach (var link in ExtractLinksFromPage(page).Where(LinkIsValid))
+            {
+                var url = $"{_website}{link}";
+                if (!_visitedPages.Contains(url))
+                {
+                    _visitedPages.Add(url);
+                    yield return url;
+                }
+            }
+        }
 
-            var result = new List<string>();
+        private IEnumerable<string> ExtractLinksFromPage(string page)
+        {
+            var hrefPattern = @"<a href\s*=\s*(?:[""'](?<1>[^""']*)[""']|(?<1>[^>\s]+))";
 
             Match regexMatch = Regex.Match(page, hrefPattern, RegexOptions.IgnoreCase);
             while (regexMatch.Success)
             {
-                var link = regexMatch.Groups[1].Value;
-                if (link.Length > 1 && link.StartsWith("/"))
-                {
-                    var url = $"{_website}{link}";
-                    if (!_visitedPages.Contains(url))
-                    {
-                        result.Add(url);
-                        _visitedPages.Add(url);
-                    }
-                }
+                yield return regexMatch.Groups[1].Value;
                 regexMatch = regexMatch.NextMatch();
             }
-            return result;
+        }
+
+        private bool LinkIsValid(string link)
+        {
+            return link != null && link.Length > 1 && link.StartsWith("/");
         }
 
         private void PersistToFile(string url, string content)
